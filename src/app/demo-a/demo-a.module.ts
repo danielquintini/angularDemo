2e9fa63e79164a6165b2d51c8cc457a65750febd
Quintini, Daniel E <daniel.e.quintini@accenture.com>
2018-01-30 12:00:35 -0500

    Added list of sample grids in page created without shell under route 'demo-a/page-a'.

--- src/app/demo-a/demo-a.module.ts
+++ src/app/demo-a/demo-a.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { IonicModule } from 'ionic-angular';
 import { PageARoutingModule } from './page-a/page-a-routing.module';
 import { PageAComponent } from './page-a/page-a.component';
 
 @NgModule({
   imports: [
     CommonModule,
-    PageARoutingModule
+    IonicModule,
+    PageARoutingModule,
   ],
   declarations: [
   	PageAComponent
